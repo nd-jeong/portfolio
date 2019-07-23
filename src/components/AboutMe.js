4e72c015cbf41825265ae3ac64c61525292daa56
Andy Jeong <unjin89@gmail.com>
2019-07-23 17:41:45 -0400

    changed resume link

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -8,7 +8,7 @@ function AboutMe() {
                 <p className='brand-statement'>Having worked mostly with technology and accompanied by an unquenchable thirst for learning, software engineering is a natural fit. I revel in the opportunity to learn about and work with new technologies. As an ambivert, I am able to adapt to any situation and that, coupled with my penchant for tackling problems head on are essential for keeping focus on the task at hand. </p>
             </div>
             <div>
-                <a href='https://drive.google.com/file/d/1YEQ-nnZyhR-Qgdh_cU3xP1blEdu3_M4m/view?usp=sharing' target='_blank' className='resume-link'>View my resume</a>
+                <a href='https://drive.google.com/file/d/1O9UKTH6NRTtEd1YYZ2Cs_GnX0u9kZyRI/view?usp=sharing' target='_blank' className='resume-link'>View my resume</a>
             </div>
         </div>
     )
