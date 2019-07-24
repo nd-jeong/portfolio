21a7cf1077b5edf6c2795ae2d7f6f65eb8bcf860
Andy Jeong <unjin89@gmail.com>
2019-07-24 15:21:59 -0400

    Updates

---
+++ precache-manifest.245e1ad2cf2fce9019f6853deea4cd6b.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1d9821e38da0bc790ae95f6585cf806",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b249b462ad6b2c78471f",
+    "url": "/portfolio/static/css/main.f3fe41d5.chunk.css"
+  },
+  {
+    "revision": "a7719d04508d296bf734",
+    "url": "/portfolio/static/js/2.8ebb8645.chunk.js"
+  },
+  {
+    "revision": "b249b462ad6b2c78471f",
+    "url": "/portfolio/static/js/main.41f6ee62.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "e4f2d6b028d93b8c7988d2132bf13ca2",
+    "url": "/portfolio/static/media/github.e4f2d6b0.svg"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/portfolio/static/media/linkedin.14f7b675.svg"
+  },
+  {
+    "revision": "abb2d9c1f69a252a263eac4311ce7989",
+    "url": "/portfolio/static/media/logo.abb2d9c1.png"
+  },
+  {
+    "revision": "e72030946c16d28c013c7bb74fa23d9d",
+    "url": "/portfolio/static/media/profile.e7203094.jpg"
+  },
+  {
+    "revision": "12737bce5dd1e4b3e1557188467dba48",
+    "url": "/portfolio/static/media/project-1-1.12737bce.png"
+  },
+  {
+    "revision": "2682b79a82d7a0189f8c3612a39e77d1",
+    "url": "/portfolio/static/media/project-1-2.2682b79a.png"
+  },
+  {
+    "revision": "051b3d4643ae0bb0b52509f505e3070b",
+    "url": "/portfolio/static/media/project-2-1.051b3d46.png"
+  },
+  {
+    "revision": "b1911abc409030bf2c7c6aa611ce0640",
+    "url": "/portfolio/static/media/project-2-2.b1911abc.png"
+  },
+  {
+    "revision": "e43d6c0e6470017dee710a076a703419",
+    "url": "/portfolio/static/media/project-3-1.e43d6c0e.png"
+  },
+  {
+    "revision": "ac4069f52a1a1c058f2277572958c5a7",
+    "url": "/portfolio/static/media/project-3-2.ac4069f5.png"
+  }
+]);
